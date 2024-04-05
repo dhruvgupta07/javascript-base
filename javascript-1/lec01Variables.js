92122308365d7b17de92a825ca2015ba01b17262
dhruvgupta07 <139675824+dhruvgupta07@users.noreply.github.com>
2024-04-05 05:30:47 +0000

    newfiles

---
+++ javascript-1/lec01Variables.js
@@ -0,0 +1,8 @@
+//learning use of const,let,var etc.
+const studentName="Dhruv"
+let studentAge="22"
+var studentFname="Atul Gupta"//never use it bcz it dosent follow scope property
+studentClass="MCA"//not a good practice
+
+console.table([studentName,studentAge,studentFname,studentClass])
+console.log(studentClass);
